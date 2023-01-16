fadb8b2690e3a8af52860dd3c37064544c7ccf91
azamfauzi <ibaneznuke@gmail.com>
2023-01-16 18:09:19 +0800

    Test Layout

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Family Hierarchy'),
     );
   }
 }
@@ -58,12 +58,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-    });
-  }
-
-  void _decreamentCounter() {
-    setState(() {
-      _counter--;
     });
   }
 
